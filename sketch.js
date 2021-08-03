0f200b08e63e9e2085b214b92219a7eff85f71fb
ayushsahu1134 <84576944+ayushsahu@users,noreply.github.com>
2021-08-03 16:51:16 +0530

    position

--- sketch.js
+++ sketch.js
@@ -1,24 +1,33 @@
 var ball;
+var dbball;
+var position;
 
 function setup(){
+database=firebase.database();
+console.log(database);
+dbball=database.ref("ball/position");
+dbball.on("value",readPosition,showError);
+
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    dbball = createSprite(250,250,10,10);
+    dbball.shapeColor = "red";
 }
 
 function draw(){
     background("white");
+
+    if(position !== undefined)
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
@@ -26,4 +35,28 @@ function draw(){
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+    
+
+}
+
+
+
+
+
+function readPosition(data){
+position = data.val();
+console.log(position);
+dbball.x = position.x;
+dbball.y = position.y;
+}
+
+function showError(){
+    console.log("Ayush");
 }
